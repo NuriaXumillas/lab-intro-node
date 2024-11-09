9a0e7118eab37878d7b7e74ddf25cc1502cf83c8
Nuria <tu.email@ejemplo.com>
2024-11-09 11:40:20 +0100

    hecho

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+   
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length; 
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos < 0 || pos >= this.items.length) {
+      throw new Error("OutOfBounds");
+    }
+    else {
+      return this.items[pos]
+    }
+  }
 
-  max() {}
+  max() {
+  if (this.items.length === 0) {
+    throw new Error("EmptySortedList");
+  }
+  else {
+   return  Math.max(...this.items);
+  }}
 
-  min() {}
+  min() {
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    else {
+      return  Math.min(...this.items);
+    }
+  }
 
-  sum() {}
+  sum() {
+    let sumaTotal = this.items.reduce((a, b) => a + b, 0);
+    return sumaTotal
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length == 0){throw new Error("Empty list")}
+    return this.items.reduce((sum, value) => sum + value, 0) / this.length;
+  }
 }
 
+const list = new SortedList();
 module.exports = SortedList;
